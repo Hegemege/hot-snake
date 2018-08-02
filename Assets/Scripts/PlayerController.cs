880d34587451e9f3e7ed8a95414c4202ba2f8fee
Henri Niva <hegemege@gmail.com>
2018-08-02 22:08:19 +0300

    Basic movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,25 +5,72 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float Speed;
+    public float SphereRadius;
     public float DistanceFromGround;
+    public float TurningSensitivity;
 
     public LayerMask GroundLayerMask;
 
-    private Vector3 _velocity;
-
     void Awake()
     {
-        _velocity = new Vector3(1f, 0f, 0f) * Speed;
-
+        // Snap to ground + offset
+        SnapToGround();
+        SnapRotation();
     }
 
-    void Update()
+    void FixedUpdate()
     {
         // Sphere is always at origin
+        Move();
+        SnapToGround();
+    }
+
+    private void Move()
+    {
+        var dt = Time.fixedDeltaTime;
+        // Move along velocity vector, snap to ground at new location
+        var newLocation = transform.position + transform.forward * Speed * dt;
+        transform.position = newLocation;
+
+        // Rotate so upvector is out from the planet
+        SnapRotation();
+
+        // Handle turning input 
+        TurningInput();
+
+        SnapToGround();
     }
 
     /// <summary>
-    /// Returns the distance to ground at current location
+    /// Snaps the rotation such that up vector is always pointing from the sphere
+    /// </summary>
+    private void SnapRotation()
+    {
+        var groundNormal = GroundPosition(transform.position.normalized * SphereRadius * 2f).normalized;
+        var newForward = Vector3.ProjectOnPlane(transform.forward, groundNormal);
+        transform.localRotation = Quaternion.LookRotation(newForward, groundNormal);
+    }
+
+    private void TurningInput()
+    {
+        var dt = Time.fixedDeltaTime;
+        var horizontalInput = Input.GetAxis("Horizontal") * TurningSensitivity * dt;
+
+        // Rotate around local Y axis based on the input
+        transform.localRotation = Quaternion.AngleAxis(horizontalInput, transform.up) * transform.localRotation;
+    }
+
+    /// <summary>
+    /// Snaps the object onto the sphere
+    /// </summary>
+    private void SnapToGround()
+    {
+        var groundPoint = GroundPosition(transform.position.normalized * SphereRadius * 2f); // Elevated current ground position
+        transform.position = groundPoint.normalized * (SphereRadius + DistanceFromGround);
+    }
+
+    /// <summary>
+    /// Returns the point on the ground sphere under the given point
     /// </summary>
     private Vector3 GroundPosition(Vector3 from)
     {
