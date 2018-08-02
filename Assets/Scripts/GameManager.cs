4624b7d191da91f228185bf26ce137f9b036e08a
Henri Niva <hegemege@gmail.com>
2018-08-03 00:59:04 +0300

    Start work on snake segments

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,6 +14,8 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    public SnakeSegmentPool SnakeSegmentPool;
+
     void Awake()
     {
         // Setup singleton
---
+++ Assets/Scripts/SnakeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeController : MonoBehaviour
+{
+    public GameObject SegmentContainer;
+    public int InitialSegmentCount;
+    public float DistanceBetweenSegments;
+
+    private int _segmentCount;
+    private List<GameObject> _segments;
+
+    void Awake()
+    {
+        _segmentCount = InitialSegmentCount;
+
+        for (var i = 0; i < InitialSegmentCount; i++)
+        {
+            // Spawn and initialize new segment
+            var newSegment = GameManager.Instance.SnakeSegmentPool.GetPooledObject();
+            newSegment.transform.parent = SegmentContainer.transform.parent;
+            newSegment.transform.localPosition = Vector3.zero;
+            newSegment.transform.localRotation = Quaternion.identity;
+            newSegment.transform.localScale = Vector3.one;
+
+            _segments.Add(newSegment);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Update the location of all segments
+        // Go from last to first
+        for (var i = _segmentCount - 1; i >= 0; i--)
+        {
+            // Move towards previous segment, snap to max distance if too far
+
+        }
+    }
+}
