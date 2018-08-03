d6b9ca268eebee3343cab467f028187d0cd3c1dc
Henri Niva <hegemege@gmail.com>
2018-08-03 19:56:04 +0300

    Random tree textures

---
+++ Assets/Scripts/RandomTexture.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomTexture : MonoBehaviour
+{
+    public List<Texture> TexturePool;
+
+    private MeshRenderer mr;
+
+    void Awake()
+    {
+        mr = GetComponentInChildren<MeshRenderer>();
+
+        var randomTexture = TexturePool[Random.Range(0, TexturePool.Count - 1)];
+        mr.material.SetTexture("_MainTex", randomTexture);
+    }
+}
